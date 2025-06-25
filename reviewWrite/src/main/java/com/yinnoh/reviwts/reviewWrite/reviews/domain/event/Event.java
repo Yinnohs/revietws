4529dea9f63e1beae5da13099f51a48bfe9f1916
yinnohs <joistleon@gmail.com>
2025-06-25 23:43:07 +0100

    Refactor (review-writer): updated accesors

--- reviewWrite/src/main/java/com/yinnoh/reviwts/reviewWrite/reviews/domain/event/Event.java
+++ reviewWrite/src/main/java/com/yinnoh/reviwts/reviewWrite/reviews/domain/event/Event.java
@@ -3,10 +3,10 @@ package com.yinnoh.reviwts.reviewWrite.reviews.domain.event;
 import java.time.LocalDateTime;
 
 public abstract class Event<T> {
-    String eventId;
-    String EventName;
-    T data;
-    LocalDateTime createdAt;
+    public String eventId;
+    public String EventName;
+    public T data;
+    public LocalDateTime createdAt;
 
     public abstract void executeEvent();
 }
