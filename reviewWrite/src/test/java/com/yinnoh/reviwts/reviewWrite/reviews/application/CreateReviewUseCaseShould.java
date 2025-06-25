c670c9592b1ff14ddca046bec1caf7825eb098d8
yinnohs <joistleon@gmail.com>
2025-06-25 23:43:35 +0100

    Test (review-writer): added Red Test

--- reviewWrite/src/test/java/com/yinnoh/reviwts/reviewWrite/reviews/application/CreateReviewUseCaseShould.java
+++ reviewWrite/src/test/java/com/yinnoh/reviwts/reviewWrite/reviews/application/CreateReviewUseCaseShould.java
@@ -1,5 +1,31 @@
 package com.yinnoh.reviwts.reviewWrite.reviews.application;
 
+import com.yinnoh.reviwts.reviewWrite.reviews.application.dto.CreateReviewRequest;
+import com.yinnoh.reviwts.reviewWrite.reviews.domain.ports.driver.ReviewService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Executable;
+import java.util.UUID;
+import java.util.function.Function;
+
 public class CreateReviewUseCaseShould {
 
+    @Test
+    public void generate_review(){
+        // given
+        CreateReviewRequest request = new CreateReviewRequest(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                2.5,
+                "test title",
+                "test description hello"
+        );
+        ReviewService service = Mockito.mock(ReviewService.class);
+        CreateReviewUseCase useCase = new CreateReviewUseCase(service);
+
+        // when / then
+        Assertions.assertDoesNotThrow(()-> {useCase.execute(request);});
+    }
 }
